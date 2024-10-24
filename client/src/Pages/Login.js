63ab74aae71656e3258225239271c69cc8a3f48f
Aman Sehgal <55401899+amansehgal76140@users.noreply.github.com>
2024-10-24 23:18:05 +0530

    Update Login.js

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -18,9 +18,7 @@ const setTokenAsCookie = (token) => {
   Cookies.set("token", token, {
     expires: 30,
     secure: true,
-    sameSite: "None",    // Required for cross-origin cookies
-    path: '/',           // Ensure the cookie is accessible across the entire domain
-    domain: "maketronics-lpms.onrenderer.com"
+    sameSite: "None"
   });
 };
 
